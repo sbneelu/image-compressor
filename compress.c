457121dc372e31a7097f509ed96479faad84a8b0
Neelu Saraswatibhatla <neelu@sbneelu.com>
2022-10-06 16:29:01 +0100

    Fixed some bugs and added colors to print statements

--- compress.c
+++ compress.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "shared.h"
 #include "files/files.h"
 #include "utils/memory/memory_utils.h"
@@ -16,58 +17,58 @@ int main(int argc, char **argv)
     char *output = argv[2];
     char *format_identifier = argv[3];
 
-    FILE *in = fopen(input, "r");
-    if (in == NULL)
+    if (strlen(format_identifier) != 4)
     {
-        printf("Error: could not open file %s\n", input);
+        printf("\033[1;31mError: invalid format identifier\033[0m\n");
         return 2;
-    }
-
-    FILE *out = fopen(output, "wb");
-    if (out == NULL)
-    {
-        printf("Error: could not open file %s\n", output);
-        fclose(in);
-        return 3;
-    }
-
-    image_t image = read_image(in);
-    if (image == NULL)
-    {
-        printf("Error: could not read image %s\n", input);
-        fclose(in);
-        fclose(out);
-        return 4;
     }
 
     byte format[4] = {format_identifier[0], format_identifier[1], format_identifier[2], format_identifier[3]};
     if (!is_valid_format(format))
     {
-        printf("Error: invalid format identifier %s\n", format_identifier);
-        fclose(in);
-        fclose(out);
-        free_image(image);
-        return 5;
+        printf("\033[1;31mError: invalid format identifier\033[0m\n");
+        return 3;
     }
 
+    printf("Opening file %s for reading...\n", input);
+    FILE *in = fopen(input, "r");
+    if (in == NULL)
+    {
+        printf("\033[1;31mError: could not open file %s\033[0m\n", input);
+        return 4;
+    }
+    
+    image_t image = read_image(in);
+    fclose(in);
+    if (image == NULL)
+    {
+        printf("\033[1;31mError: could not read image %s\033[0m\n", input);
+        return 5;
+    }
+    
+    printf("Compressing image...\n");
     compressed_file_t compressed_file = compress(image, format, argc - 4, argv + 4);
+    free_image(image);
     if (compressed_file == NULL)
     {
-        printf("Error: could not compress file %s. There may be additional information above.\n", input);
-        fclose(in);
-        fclose(out);
-        free_image(image);
+        printf("\033[1;31mError: could not compress file %s. There may be additional information above.\033[0m\n", input);
         return 6;
     }
 
+    printf("Opening file %s for writing...\n", output);
+    FILE *out = fopen(output, "wb");
+    if (out == NULL)
+    {
+        printf("\033[1;31mError: could not open file %s\033[0m\n", output);
+        return 7;
+    }
+
+    printf("Writing compressed file...\n");
     write_compressed_file(out, compressed_file);
 
-    fclose(in);
     fclose(out);
-
-    free_image(image);
     free_compressed_file(compressed_file);
 
-    printf("Compressed %s to %s!\n", input, output);
+    printf("\033[1;32mCompressed %s to %s!\033[0m\n", input, output);
     return 0;
 }
--- decompress.c
+++ decompress.c
@@ -15,63 +15,60 @@ int main(int argc, char **argv)
     char *input = argv[1];
     char *output = argv[2];
 
+    printf("Opening file %s for reading...\n", input);
     FILE *in = fopen(input, "rb");
     if (in == NULL)
     {
-        printf("Error: could not open file %s\n", input);
+        printf("\033[1;31mError: could not open file %s\033[0m\n", input);
         return 2;
     }
 
-    FILE *out = fopen(output, "w");
-    if (out == NULL)
+    printf("Reading compressed file...\n");
+    compressed_file_t compressed_file = read_compressed_file(in);
+    fclose(in);
+    if (compressed_file == NULL)
     {
-        printf("Error: could not open file %s\n", output);
-        fclose(in);
+        printf("\033[1;31mError: could not read compressed file %s\033[0m\n", input);
         return 3;
     }
 
-    compressed_file_t compressed_file = read_compressed_file(in);
-    if (compressed_file == NULL)
-    {
-        printf("Error: could not read compressed file %s\n", input);
-        fclose(in);
-        fclose(out);
-        return 4;
-    }
 
     if (!is_valid_format(compressed_file->format_identifier))
     {
-        printf("Error: invalid format identifier - compressed file may be corrupted\n");
-        fclose(in);
-        fclose(out);
+        printf("\033[1;31mError: invalid format identifier - compressed file may be corrupted\033[0m\n");
         free_compressed_file(compressed_file);
         return 5;
     }
 
     image_t image = decompress(compressed_file, argc - 3, argv + 3);
+    free_compressed_file(compressed_file);
+
     if (image == NULL)
     {
-        printf("Error: could not decompress file %s. There may be additional information above.\n", input);
-        fclose(in);
-        fclose(out);
-        free_compressed_file(compressed_file);
+        printf("\033[1;31mError: could not decompress file %s. There may be additional information above.\033[0m\n", input);
         return 6;
     }
 
+    printf("Opening file %s for writing...\n", output);
+    FILE *out = fopen(output, "w");
+    if (out == NULL)
+    {
+        printf("\033[1;31mError: could not open file %s\033[0m\n", output);
+        return 4;
+    }
+
+    printf("Writing decompressed image...\n");
     bool success = write_image(out, image);
 
-    fclose(in);
     fclose(out);
-
     free_image(image);
-    free_compressed_file(compressed_file);
-
+    
     if (!success)
     {
-        printf("Error: Could not write decompressed image.");
+        printf("\033[1;31mError: could not write decompressed image\033[0m\n");
         return 7;
     }
 
-    printf("Decompressed %s to %s!\n", input, output);
+    printf("\033[1;32mDecompressed %s to %s!\033[0m\n", input, output);
     return 0;
 }
